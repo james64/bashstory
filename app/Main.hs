1ca162d8de53c0cd731976bca79702c6c1445178
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-05-26 11:56:05 +0200

    dummy copy in to out

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
+import System.IO
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    iseof <- hIsEOF stdin
+    if iseof
+        then return ()
+        else do
+            line <- getLine
+            putStrLn line
+            main
