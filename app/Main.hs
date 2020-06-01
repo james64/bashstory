e06f3c9f661a712f56f2bead29b4ce8a07073ae4
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-06-01 16:08:29 +0200

    print this session only

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,35 @@
 module Main where
 
-import System.IO
-import Lib
-import GHC.IO.Encoding
+import System.IO ( stdin, hSetEncoding )
+import System.Environment ( getArgs )
+import System.Exit ( exitWith, ExitCode(ExitFailure) )
+import Data.List ( foldl', isSuffixOf )
+import GHC.IO.Encoding ( mkTextEncoding )
 
-loop :: IO ()
-loop = do
-    iseof <- hIsEOF stdin
-    if iseof
-        then return ()
-        else do
-            line <- getLine
-            putStrLn line
-            loop
+sessionDelimiter :: String
+sessionDelimiter = "### Session ###"
+
+currentSession :: [String] -> [String]
+currentSession = reverse . foldl' lastS []
+    where
+        lastS :: [String] -> String -> [String]
+        lastS _ l | isDelim l = []
+        lastS s l             = l : s
+
+        isDelim = isSuffixOf sessionDelimiter
+
+currSessIO :: IO ()
+currSessIO = do
+    input <- getContents
+    mapM_ putStrLn $ id $ currentSession (lines input)
 
 main :: IO ()
 main = do
     enc <- mkTextEncoding "UTF-8//IGNORE"
     hSetEncoding stdin enc
-    loop
+    args <- getArgs
+    case args of
+        []       -> interact id
+        ["curr"] -> currSessIO
+        _        -> exitWith (ExitFailure 1)
+
