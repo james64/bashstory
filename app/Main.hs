9438116f435625307d35534c0b1bc3261a01840b
Jakub Dubovsky <jakub.dubovsky@seznam.cz>
2020-06-06 19:09:49 +0200

    add argument parsing library

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,37 @@ module Main where
 
 import System.IO ( stdin, hSetEncoding )
 import System.Environment ( getArgs )
-import System.Exit ( exitWith, ExitCode(ExitFailure) )
 import Data.List ( foldl', isSuffixOf )
 import GHC.IO.Encoding ( mkTextEncoding )
+
+-- arg parsing
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+-- Argument parsing --
+data Options = Options { current :: Bool }
+
+currentP :: Parser Bool
+currentP = switch ( long "current" <> short 'c' <> help "Commands from current session only" )
+
+options :: Parser Options
+options = Options <$> currentP
+
+opts :: ParserInfo Options
+opts = info (options <**> helper)
+  (  fullDesc
+  <> progDesc "Print history list"
+  <> header "this is helper header" )
+
+run :: Options -> IO ()
+run (Options False) = interact id
+run (Options True)  = currSessIO
+
+main :: IO ()
+main = do
+  enc <- mkTextEncoding "UTF-8//IGNORE"
+  hSetEncoding stdin enc
+  run =<< execParser opts
 
 sessionDelimiter :: String
 sessionDelimiter = "### Session ###"
@@ -22,14 +50,3 @@ currSessIO :: IO ()
 currSessIO = do
     input <- getContents
     mapM_ putStrLn $ id $ currentSession (lines input)
-
-main :: IO ()
-main = do
-    enc <- mkTextEncoding "UTF-8//IGNORE"
-    hSetEncoding stdin enc
-    args <- getArgs
-    case args of
-        []       -> interact id
-        ["curr"] -> currSessIO
-        _        -> exitWith (ExitFailure 1)
-
